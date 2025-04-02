3808d51586acafa8393544c8ca112bef57756d41
chonticha0560 <chontichasth3105@gmail.com>
2025-04-02 11:49:03 +0700

    myproject/src/main/java/com/project/Notification.java

--- myproject/src/main/java/com/project/Notification.java
+++ myproject/src/main/java/com/project/Notification.java
@@ -16,17 +16,14 @@ public class Notification {
 
     public void checkNotifications() {
         LocalDateTime now = LocalDateTime.now();
-        // System.out.println("🔍 ตรวจสอบกิจกรรม เวลา: " + now); // เช็คว่าฟังก์ชันทำงาน
 
         Iterator<Map.Entry<String, LocalDateTime>> iterator = task.getTaskTimes().entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, LocalDateTime> entry = iterator.next();
-            // System.out.println("⏰ เช็คกิจกรรม: " + entry.getKey() + " -> " +
-            // entry.getValue()); // Debug ข้อมูลกิจกรรม
 
             if (entry.getValue().isBefore(now) || entry.getValue().isEqual(now)) {
-                // System.out.println("🚨 แจ้งเตือน: " + e ntry.getKey()); // เช็คว่าถึงเวลาจริง
-                showNotification(entry.getKey()); // เรียกแจ้งเตือน
+
+                showNotification(entry.getKey()); 
                 iterator.remove();
                 task.getScheduleList().getItems().remove(entry.getKey());
                 break;
@@ -36,7 +33,7 @@ public class Notification {
 
     public void showNotification(String task) {
         Platform.runLater(() -> {
-            // System.out.println("📢 แสดงป๊อปอัพ: " + task); // Debug
+
 
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("แจ้งเตือนกิจกรรม");
